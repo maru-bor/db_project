ce87a732b2cdf4feffaada6021979c637b3850f3
marie <borisova.marie1@gmail.com>
2025-02-08 14:56:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace db_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
