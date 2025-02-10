8dac7ab898073b6684a273af6376c6837a4a8320
marie <borisova.marie1@gmail.com>
2025-02-10 21:55:40 +0100

    Add class DatabaseSingleton and add method for connecting to database

---
+++ DatabaseSingleton.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_project
+{
+    internal class DatabaseSingleton
+    {
+        private static SqlConnection? conn = null;
+
+
+        public static SqlConnection GetConnInstance()
+        {
+            if(conn == null)
+            {
+                SqlConnectionStringBuilder consStringBuilder = new SqlConnectionStringBuilder();
+                consStringBuilder.UserID = ConfigurationManager.AppSettings["UserID"]; 
+                consStringBuilder.Password = ConfigurationManager.AppSettings["Password"];
+                consStringBuilder.InitialCatalog = ConfigurationManager.AppSettings["InitialCatalog"];
+                conn = new SqlConnection(consStringBuilder.ConnectionString);
+                conn.Open();
+                Console.WriteLine("Connected");
+                
+
+            }
+            return conn;
+        }
+
+    }
+}
