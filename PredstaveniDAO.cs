b169acec3197e806f8b0a35122c51785813f7bab
marie <borisova.marie1@gmail.com>
2025-02-14 19:34:38 +0100

    Add class PredstaveniDAO

---
+++ PredstaveniDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_project
+{
+    internal class PredstaveniDAO : DAOInterface<Predstaveni>
+    {
+        public void Delete(Predstaveni element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Predstaveni> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Predstaveni GetByValueName(params string[] names)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Predstaveni element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Predstaveni previousElement, Predstaveni updatedElement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
